7b5c727dce9300340cbb957a65ae3a43810e7e58
franarayah <franarayah@gmail.com>
2025-01-06 21:15:20 -0300

    Fixed database script and create model relationated to user class

--- dbsecurity.sql
+++ dbsecurity.sql
@@ -2,10 +2,10 @@
 -- Script to work with Spring Security learning project
 CREATE TABLE user_login(
 	id				serial PRIMARY KEY,
-	name			varchar(50) UNIQUE NOT NULL,
+	name			varchar(100) UNIQUE NOT NULL,
 	email 		varchar(100) UNIQUE NOT NULL,
-	password 	varchar(255) NOT NULL,
-	ROLE			varchar(20) NOT null
+	password 	varchar(50) NOT NULL,
+	ROLE			varchar(50) NOT null
 );
 
 INSERT INTO user_login (name, email, password , role) VALUES
--- spring/src/main/java/com/segurito/spring/models/User.java
+++ spring/src/main/java/com/segurito/spring/models/User.java
@@ -1,4 +1,86 @@
 package com.segurito.spring.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_login")
 public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false, unique = true, length = 100)
+  private String name;
+
+  @Column(name = "email", nullable = false, unique = true, length = 100)
+  private String email;
+
+  @Column(name = "password", nullable = false, unique = true, length = 50)
+  private String password;
+
+  @Column(name = "role", nullable = false, unique = true, length = 50)
+  private String role;
+
+  public User() {
+  }
+
+  public User(Long id, String name, String email, String password, String role) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            ", password='" + password + '\'' +
+            ", role='" + role + '\'' +
+            '}';
+  }
 }
